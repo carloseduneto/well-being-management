ca6a68f5b3a8de38b54f5f055a6260ca3efe836b
Carlos Neto <cecn2710@gmail.com>
2025-10-21 12:02:53 -0300

    feat: corrigir caminho do manifest e atualizar botão de instalação no index.html; ajustar cache no service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ self.addEventListener("install", (event) => {
   console.log("Service Worker instalado");
   event.waitUntil(
     caches.open("app-cache").then((cache) => {
-      return cache.addAll(["/", "/index.html", "/style.css", "/script.js"]);
+      return cache.addAll(["/", "/index.html", "/style.css","/file", "/file/script.js"]);
     })
   );
 });
@@ -10,7 +10,11 @@ self.addEventListener("install", (event) => {
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+      return response || fetch(event.request).catch(() => {
+        return caches.match("/index.html"); // fallback offline
+        
+      });
     })
   );
 });
+
