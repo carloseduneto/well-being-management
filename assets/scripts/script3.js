658831893b995f28025f5accf95f6e7af0514cff
Carlos Neto <cecn2710@gmail.com>
2025-10-28 13:46:10 -0300

    .

--- assets/scripts/script3.js
+++ assets/scripts/script3.js
@@ -14,7 +14,7 @@ async function getExerciseData() {
         console.log(element.exercicio.nome);
         allExercises += '<a href="./exercicio-detalhes.html">' +
               '<div class="exerciseCard">'+
-               ' <div class="card" data-progress=70>'+
+               '<div class="card" data-progress="60">'+
                   '<div class="box">'+
                     '<div class="percent">'+
                       '<svg>'+
@@ -34,13 +34,32 @@ async function getExerciseData() {
               '</div>'+
             '</a>';
         
-    });
-    
-    const exerciseID = document.getElementById("exerciseData");
-    exerciseID.innerHTML = allExercises;    
-  console.log("Teste");
+          });
+          
+          const exerciseID = document.getElementById("exerciseData");
+          exerciseID.innerHTML = allExercises;    
+          console.log("Teste");
+          animateProgressCircles();
+}
+getExerciseData();
+
+function animateProgressCircles() {
+document.querySelectorAll(".card").forEach(card => {
+        console.log(card); 
+        const circle = card.querySelector("circle:nth-child(2)");
+        const progress = card.dataset.progress;
+        if (progress != 0) {
+          const dashoffset = 180 - (180 * progress) / 100;
+
+          // força o início da animação (0%)
+          circle.style.strokeDashoffset = 180;
+          requestAnimationFrame(() => {
+            circle.style.strokeDashoffset = dashoffset;
+          });
+        } else {
+          circle.style.stroke = "#E3E3E3";
+        }
+      });
 }
 
-// getExerciseData();
-
-
+animateProgressCircles();
