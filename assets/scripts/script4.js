83f189edf0f7a1fe844978cd8c04df702db8aed2
Carlos Eduardo Neto <112590775+carloseduneto@users.noreply.github.com>
2025-11-07 17:43:16 -0300

    teste script

---
+++ assets/scripts/script4.js
@@ -0,0 +1,8 @@
+function test() {
+    const msg = document.getElementById("message");
+    msg.textContent = "Script 4 carregado e executado!";
+    console.log("Script 4 executado");
+}
+
+
+test();
